defaf331ac774cd6d7b21040aa3f16cd5a4edc88
kornienkokostia <kornienkokostia@gmail.com>
2023-02-07 19:18:27 +0200

    fix: removed active className

--- rsclone/src/pages/title-page/TitlePage.tsx
+++ rsclone/src/pages/title-page/TitlePage.tsx
@@ -5,12 +5,12 @@ import { useTitle } from '../../hooks/titleData';
 export const TitlePage = () => {
   const {isTvShow, title, cast, images, videos, reviews, similar} = useTitle()
 
-  console.log(title)
-  console.log(cast)
-  console.log(images)
-  console.log(videos)
-  console.log(reviews)
-  console.log(similar)
+  // console.log(title)
+  // console.log(cast)
+  // console.log(images)
+  // console.log(videos)
+  // console.log(reviews)
+  // console.log(similar)
   
   
   // const mainTariler = videos!.results.filter(el => el.type === 'Trailer')
