745302eddc7c5b8697af1ec92c3077f9d9280260
kornienkokostia <kornienkokostia@gmail.com>
2023-02-08 17:12:26 +0200

    fix

--- rsclone/src/pages/home-page/HomePage.tsx
+++ rsclone/src/pages/home-page/HomePage.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { MainCarousel } from '../../components/carousel/carousel'
+import { Link } from "react-router-dom"
+import '../home-page/homePage.scss';
+import { MainCarousel } from "../../components/carousel/carousel";
 
-export const HometPage = () => {
+export const HomePage = () => {
   return (
     <>
       <MainCarousel/>
+      <Link to='/title/tt4154796' className="test-title">Movie</Link>
+      <Link to='/title/tt13443470' className="test-title">Show</Link>
+      
     </>
+    
   )
 }
