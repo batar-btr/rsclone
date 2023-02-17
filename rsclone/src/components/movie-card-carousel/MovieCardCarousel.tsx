fc99fb2e19893c41908e30de69fa704b4a666791
kornienkokostia <kornienkokostia@gmail.com>
2023-02-17 13:31:56 +0200

    fix: menu carusel rating

--- rsclone/src/components/movie-card-carousel/MovieCardCarousel.tsx
+++ rsclone/src/components/movie-card-carousel/MovieCardCarousel.tsx
@@ -113,7 +113,7 @@ const MovieCardCarousel: FC<MovieCardCarouselProps> = ({ topTitle, subTitle, typ
 
     return (
       <div className='movie-card'>
-        <AddFlag checked={select} loading={loading} onClick={addMovieHandler}></AddFlag>
+        <AddFlag checked={isAdded} loading={loading} onClick={addMovieHandler}></AddFlag>
         <div className="img-wrap">
           <Link to={`/${type}/${id}`}>
             <img src={imgPath} alt="poster" />
@@ -127,7 +127,10 @@ const MovieCardCarousel: FC<MovieCardCarouselProps> = ({ topTitle, subTitle, typ
                 <Star fill='#f5c518' width='13' height='13'></Star>
                 <span>{rate.toFixed(1)}</span>
               </div>
-              <button className='rate-btn' onClick={toggle}><StrokeStar fill='#fff' width='14' height='14'></StrokeStar></button>
+              <button className='rate-btn' onClick={toggle}>
+                {rating ? <Star fill='#5799ef' width='14' height='14' /> : <StrokeStar fill='#fff' width='14' height='14' />}
+                {rating && ` ${rating}`}
+              </button>
               <Modal isShowing={isShowing} hide={toggle}>
                 <RateBox title={title} hide={toggle} id={id} type={type}></RateBox>
               </Modal>
@@ -136,7 +139,7 @@ const MovieCardCarousel: FC<MovieCardCarouselProps> = ({ topTitle, subTitle, typ
           </div>
           <div className='info-block__bottom'>
             <button className='watch-list-btn' onClick={addMovieHandler}>
-            {!loading && <><span>{select ? 
+            {!loading && <><span>{isAdded ? 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="watch-list-btn-added" viewBox="0 0 24 24" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0 .984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4.984.984 0 0 0-1.4 0L9 16.2z"></path></svg> : 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="watch-list-btn-add" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>}
                 </span><>Watchlist</></>}
